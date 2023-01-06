6dc2ad30918b0e82657931146edcac04406c22f4
Ostap Bobrys <117922739+OstapBobrys@users.noreply.github.com>
2023-01-06 21:42:30 +0200

    Create Alert.jsx

---
+++ src/components/Alert.jsx
@@ -0,0 +1,30 @@
+import { useGlobalState } from "../store"
+import { FaRegTimesCircle } from "react-icons/fa"
+import { BsCheck2Circle } from "react-icons/bs"
+const Alert = () => {
+   const [alert] = useGlobalState('alert')
+  return (
+    <div className={`fixed top-0 left-0 w-screen h-screen flex
+    items-center justify-center bg-black bg-opacity-50 
+    transform transition-transform duration-300 
+    ${alert.show ? 'scale-100' : 'scale-0'} `}
+    >
+
+        <div className="flex flex-col justify-center items-center 
+        gradient-bg-addNFT shadow-xl shadow-[#e329da] rounded-xl
+        min-w-min py-3 px-10">
+
+            {alert.color === 'red' ? (
+                <FaRegTimesCircle className="text-red-600 text-4xl" />        
+            ) : (
+                <BsCheck2Circle className="text-green-600 text-4xl" />
+            )}
+                <p className="text-white">{alert.msg}</p>
+
+            
+        </div>
+    </div>
+  )
+}
+
+export default Alert
